90bd5583b997efaa649ebb08e6ed59ce230fb945
Kasia Sosulska <69992@student.ue.poznan.pl>
2022-05-27 15:00:29 +0200

    cwiczonko

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+class MenadzerPolaczen:
+    def __init__(self, filename):
+        self.filename=filename
+        self.data_dict=self.read_data()
+
+    def read_data(self):
+        calls_dict_sum=dict()
+        with open(self.filename,'r') as fin:
+            reader=csv.reader(fin,delimiter=',')
+            header=next(reader)
+
+            for row in reader:
+                from_subsr=int(row[0])
+                if from_subsr not in calls_dict_sum:
+                    calls_dict_sum[from_subsr]=0
+                calls_dict_sum[from_subsr]+=1
+            return calls_dict_sum
+
+    def pobierz_najczesciej_dzwoniacego(self):
+        return max(self.data_dict.items(), key=lambda x:x[1])
+
+class SprawdzDzwoniacegoTest(TestCase):
+    def test_czy_abonent_najczesciej_dzwoniacy_rozpoznany_poprawnie(self):
+        mp=MenadzerPolaczen("phoneCalls.csv")
+        wynik=mp.pobierz_najczesciej_dzwoniacego()
+        self.assertEqual((226,5),wynik)
 
 if __name__ == "__main__":
-    pass
+    print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
